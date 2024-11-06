e7d9316f5f095ec367b8efe1337cde12d52d1e64
Jo Ao <jpmachacaz@gmail.com>
2024-11-06 21:27:59 +0100

    Add prime framework

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,83 @@
-<script setup lang="ts">
-import { ref } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Connector from './components/connector.vue';
+import Status from './components/status.vue';
+import Main from './components/main/main.vue';
+import Files from './components/files.vue';
 
-let socket: WebSocket | null = null;
-const websocketUrl = ref('');
-const gcodeCommand = ref('');
+export default defineComponent({
+  name: 'Root',
+  components: {
+    Connector,
+    Status,
+    Main,
+    Files
+  },
+  data: () => ({
 
-function connectSocketServer() {
-  // if (!websocketUrl.value) {
-  //   console.error('Websocket URL is required');
-  //   return;
-  // }
-
-  socket = new WebSocket(websocketUrl.value || 'ws://127.0.0.1:9002');
-
-  socket.onopen = () => {
-    console.log('WebSocket connected');
-  };
-
-  socket.onerror = (error) => {
-    console.error('WebSocket error:', error);
-  };
-
-  socket.onclose = () => {
-    console.log('WebSocket closed');
-  };
-}
-
-function sendCommand(type: string, command: string) {
-  if (!socket || socket.readyState !== WebSocket.OPEN) {
-    console.error('WebSocket is not connected');
-    return;
-  }
-
-  const message = {
-    message_type: type,
-    message: command
-  }
-
-  socket.send(JSON.stringify(message));
-}
+  }),
+})
 </script>
 
 <template>
-  <div class="container">
-    <h1>Websocket connection</h1>
+  <div class="main-container">
+    <div class="left-container">
+      <Accordion multiple>
+        <AccordionPanel value="0">
+          <AccordionHeader> Connector </AccordionHeader>
+          <AccordionContent>
+            <div class="left-container-1">
+              <Connector />
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
 
-    <input type="text" placeholder="ws://websocket-server-url:port" v-model="websocketUrl" />
-    <button @click="connectSocketServer">Connect</button>
-    <button @click="socket?.close()">Abort connection</button>
+        <AccordionPanel value="1">
+          <AccordionHeader> Status </AccordionHeader>
+          <AccordionContent>
+            <div class="left-container-2">
+              <Status />
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
+        <AccordionPanel value="2">
+          <AccordionHeader> Files </AccordionHeader>
+          <AccordionContent>
+            <div class="left-container-3">
+              <Files />
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
+      </Accordion>
 
-    <input type="text" placeholder="Gcode command" v-model="gcodeCommand" />
-    <button @click="sendCommand('Information', gcodeCommand)">Send Command</button>
-    <button @click="sendCommand('Information', 'M115')">Get information</button>
-
-    <div id="remove-controller">
-      <!-- <input type="radio" id="one-v" name="move-length" /><label for="one">1</label>
-      <input type="radio" id="ten-v" name="move-length" /><label for="ten">10</label>
-      <input type="radio" id="hundred-v" name="move-length" /><label for="hundred">100</label> -->
-
-      <button @click="sendCommand('Movement', 'G1 X10')">Forward</button>
-      <button @click="sendCommand('Movement', 'G1 X-10')">Left</button>
-      <button @click="sendCommand('Movement', 'G1 Y10')">Right</button>
-      <button @click="sendCommand('Movement', 'G1 Y-10')">Backwards</button>
-      <button @click="sendCommand('Movement', 'G1 Z10')">Up</button>
-      <button @click="sendCommand('Movement', 'G1 Z-10')">Down</button>
+    </div>
+    <Divider layout="vertical" />
+    <div class="right-container">
+      <Main />
     </div>
   </div>
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
+.main-container {
+  display: flex;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+.left-container {
+  flex: 1;
+  /* Additional styles for left container */
+}
+
+.right-container {
+  flex: 2;
+  /* Adjust as necessary */
+  /* Additional styles for right container */
+}
+
+.left-container-1,
+.left-container-2,
+.left-container-3 {
+  /* margin-bottom: 16px; */
+  /* Spacing between sections */
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,69 @@
 import { createApp } from "vue";
-import "./styles.css";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import 'primeicons/primeicons.css'
+import "./styles.css";
+
+// Theme and styled components
+// ONLY import necessary components
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import Button from "primevue/button";
+import InputNumber from 'primevue/inputnumber';
+import InputGroup from "primevue/inputgroup";
+import InputText from "primevue/inputtext";
+import Terminal from 'primevue/terminal';
+
+import Accordion from 'primevue/accordion';
+import AccordionPanel from 'primevue/accordionpanel';
+import AccordionHeader from 'primevue/accordionheader';
+import AccordionContent from 'primevue/accordioncontent';
+
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+
+import Divider from 'primevue/divider';
+
+import Panel from 'primevue/panel';
+
+import { LineController, LineElement, PointElement, CategoryScale, LinearScale } from "chart.js/auto";
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+// Primevue
+.component('Button', Button)
+.component('InputNumber', InputNumber)
+.component('InputGroup', InputGroup)
+.component('InputText', InputText)
+.component('Terminal', Terminal)
+
+.component('Accordion', Accordion)
+.component('AccordionPanel', AccordionPanel)
+.component('AccordionHeader', AccordionHeader)
+.component('AccordionContent', AccordionContent)
+
+.component('Tabs', Tabs)
+.component('Tab', Tab)
+.component('TabList', TabList)
+.component('TabPanel', TabPanel)
+.component('TabPanels', TabPanels)
+
+.component('Divider', Divider)
+.component('Panel', Panel)
+// Chart.js
+.component('LineController', LineController)
+.component('LineElement', LineElement)
+.component('PointElement', PointElement)
+.component('CategoryScale', CategoryScale)
+.component('LinearScale', LinearScale)
+// mount App
+.mount("#app");
