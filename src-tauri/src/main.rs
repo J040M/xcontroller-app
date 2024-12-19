00c808c58edf9e7dde9ff6d842975e7fe6263e34
Partiri <5804297+Partiri@users.noreply.github.com>
2024-12-19 23:57:25 +0100

    update packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,7 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
